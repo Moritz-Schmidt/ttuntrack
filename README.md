1071311a3d256842dcb6ba0b4db81e52873a1878
Moritz Schmidt <88738242+Moritz-Schmidt@users.noreply.github.com>
2022-11-10 00:10:36 +0100

    added JSON api

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # ttuntrack
+
+a simple tool to remove the tracking information from a TikTok share URL and return the original URL.
+
+## Demo
+
+[tu.moroku.de](https://tu.moroku.de)
+
+## Usage
+
+- go to Website
+- paste TikTok Share URL
+- click on submit
+- copy the new URL
+
+or use the API:
+    
+    ```bash
+    curl -X POST -d '{"url": "https://vm.tiktok.com/ABC2D3EFG"' https://tu.moroku.de/tt-untrack
+    ```
+    
+    ```json
+    {
+        "url": "https://www.tiktok.com/@user/video/1234567891234567891"
+    }
+    ```
+
+Please use the API responsibly. If you want to use it more often, please host a copy of this project yourself.
+
+## Installation
+
+- ajust the docker-compose.yml file to your needs (e.g. remove the traefik labels, add port mapping)
+- run `docker-compose up -d`
+or:
+- run `docker build -t ttuntrack .`
+- run `docker run -d -p 80:80 --name ttuntrack ttuntrack`
+or:
+- run `pip install -r requirements.txt`
+- run `python3 app.py`
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+Copyright (c) 2022 Moritz Schmidt
+
+## Contributing
+
+Just open a pull request or write a issue :)
--- app.py
+++ app.py
@@ -1,25 +1,61 @@
-from fastapi import FastAPI, Form from fastapi.responses import 
-HTMLResponse from urllib3.util import parse_url import requests import 
-uvicorn app = FastAPI() @app.post("/tt-untrack/") async def 
-untrack_tiktok(url: str = Form()):
-	parsed_url = parse_url(url) r = requests.head(parsed_url.url) u 
-	= parse_url(r.headers["Location"]) return u.scheme + "://" + 
-	u.host + u.path
+from fastapi import FastAPI, Form, HTTPException, Body
+from fastapi.responses import HTMLResponse
+from urllib3.util import parse_url
+import requests
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import HttpUrl
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+def untrack_tiktok(url):
+    if not url:
+        raise HTTPException(status_code=400, detail="URL is required")
+    parsed_url = parse_url(url)
+    if not "tiktok.com" in parsed_url.host:
+        raise HTTPException(status_code=400, detail="Invalid URL")
+    r = requests.head(parsed_url.url) 
+    u = parse_url(r.headers["Location"])
+    return u.scheme + "://" + u.host + u.path
+
+@app.post("/tt-untrack-form/") 
+async def tt_untrack_form(url: str = Form()):
+    return untrack_tiktok(url)
+
+@app.post("/tt-untrack/")
+async def tt_untrack(url: str = Body()):
+    return {"url": untrack_tiktok(url)}
+
+
+
+
+@app.get("/", response_class=HTMLResponse) 
+async def index(): 
+    index = """<!DOCTYPE html>
+<html>
+<head>
+    <title>untrack TikTok links</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, 
+    initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<body>
+    <form action="/tt-untrack-form" method="post"> <label class="form-label" for="url"> Tik Tok share URL</label> <input
+            class="form-control" id="url" name="url" type="text"> <br /> <button class="form-control" type="submit">
+            submit </button>
+</body>
+</html> """
+    return index
 	
-@app.get("/", response_class=HTMLResponse) async def index(): index = 
-	"""
- <!DOCTYPE html> <html> <head> <title>untrack TikTok links</title> <meta 
-    charset="utf-8"> <meta name="viewport" content="width=device-width, 
-    initial-scale=1"> <link 
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" 
-    rel="stylesheet" 
-    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" 
-    crossorigin="anonymous">
-</head> <body> <form action="/tt-untrack" method="post"> <label 
-class="form-label" for="url"> Tik Tok share URL</label> <input 
-class="form-control" id="url" name="url" type="text"> <br/> <button 
-class="form-control" type="submit"> submit </button> </body> </html> """
-	return index
-	
-#if __name__ == "__main__":
-#    uvicorn.run(app, host="0.0.0.0", port=8080)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=80)
